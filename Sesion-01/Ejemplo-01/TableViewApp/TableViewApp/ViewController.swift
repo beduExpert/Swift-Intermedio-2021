1df1f41734d2e1834b6d1dba745e3c898d2f2ccc
jan-zelaznog <jan.zelaznog@gmail.com>
2021-09-02 19:30:33 -0500

    Sesiones 1 y 2
    Nuevas versiones de materiales para las sesiones 1 y 2. Version UIKit

--- Sesion-01/Ejemplo-01/TableViewApp/TableViewApp/ViewController.swift
+++ Sesion-01/Ejemplo-01/TableViewApp/TableViewApp/ViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  @IBOutlet weak var tableView: UITableView!
-  
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.delegate = self
-    tableView.dataSource = self
-    // Do any additional setup after loading the view.
-  }
-}
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    let tv = UITableView()
+    tv.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    tv.translatesAutoresizingMaskIntoConstraints = true
+    tv.backgroundColor = .green
+    tv.frame = self.view.frame.inset(by: UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0))
+    self.view.addSubview(tv)
+    
+    let lb = UILabel()
+    lb.autoresizingMask = .flexibleWidth
+    lb.translatesAutoresizingMaskIntoConstraints = true
+    lb.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 60)
+    lb.text = "¡¡FRUTAS!!"
+    lb.backgroundColor = .gray
+    lb.textAlignment = .center
+    self.view.addSubview(lb)
+    
   }
 }
